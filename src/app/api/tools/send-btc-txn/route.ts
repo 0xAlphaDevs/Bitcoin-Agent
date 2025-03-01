98e8c00dcaedb9905f2a71e8506ca30de028d939
Harsh Tyagi <writetoharshtyagi@gmail.com>
2025-03-01 21:06:52 +0530

    chore: test fee

--- src/app/api/tools/send-btc-txn/route.ts
+++ src/app/api/tools/send-btc-txn/route.ts
@@ -100,7 +100,6 @@ export async function GET(request: Request) {
 
     // set maximum fee rate
     transaction.psbt.setMaximumFeeRate(10);
-    console.log("transaction fee rate", transaction.psbt.getFeeRate());
     console.log("transaction fee", transaction.psbt.getFee());
 
     const signedTransaction = Bitcoin.addSignature({
